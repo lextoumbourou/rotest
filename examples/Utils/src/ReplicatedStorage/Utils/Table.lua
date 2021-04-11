2bb203b70bf76ac253d36cc78c27506c74cfe860
Lex Toumbourou <lextoumbourou@gmail.com>
2021-04-11 19:54:21 +1000

    Add table equals example.

--- examples/Utils/src/ReplicatedStorage/Utils/Table.lua
+++ examples/Utils/src/ReplicatedStorage/Utils/Table.lua
@@ -12,4 +12,37 @@ function TableUtils.copyTable(t)
 	return res
 end
 
+function TableUtils.tablesEqual(t1, t2)
+	if (t1 == nil or t2 == nil) and t1 ~= t2 then
+		return false
+	end
+
+	for i, v in pairs(t1) do
+		if (typeof(v) == "table") then
+			if (TableUtils.tablesEqual(v, t2[i]) == false) then
+				return false
+			end
+		else
+			if (v ~= t2[i]) then
+				return false
+			end
+		end
+	end
+
+	for i, v in pairs(t2) do
+		if (typeof(v) == "table") then
+			if (TableUtils.tablesEqual(v, t1[i]) == false) then
+				return false
+			end
+		else
+			if (v ~= t1[i]) then
+				return false
+			end
+		end
+	end
+
+	return true
+end
+
+
 return TableUtils
--- examples/Utils/src/ReplicatedStorage/Utils/Tests/Table.test.lua
+++ examples/Utils/src/ReplicatedStorage/Utils/Tests/Table.test.lua
@@ -5,8 +5,8 @@ local TableUtilTest = {}
 function TableUtilTest:copiesBasicTable()
 	local tableOriginal = {name='Lex'}
 	local sut = require(game.ReplicatedStorage.Utils.Table)
-
 	local copiedTable = sut.copyTable(tableOriginal)
+
 	copiedTable.name = 'John'
 
 	assert(tableOriginal.name == "Lex", "Original table has been modified")
@@ -16,12 +16,36 @@ end
 function TableUtilTest:copiesNestedTable()
 	local tableOriginal = {items={Hat=1}}
 	local sut = require(game.ReplicatedStorage.Utils.Table)
-
 	local copiedTable = sut.copyTable(tableOriginal)
+
 	copiedTable.items.Hat = 10
 
 	assert(tableOriginal.items.Hat == 1, "Original table has been modified")
 	assert(copiedTable.items.Hat == 10, "Updated table has not been modified")
+end
+
+function TableUtilTest:comparesEqualTables()
+	local sut = require(game.ReplicatedStorage.Utils.Table)
+
+	local isEqual = sut.tablesEqual({a=1, b=2}, {a=1, b=2})
+
+	assert(isEqual, "Tables aren't equal")
+end
+
+function TableUtilTest.comparesEqualArrays()
+	local sut = require(game.ReplicatedStorage.Utils.Table)
+
+	local isEqual = sut.tablesEqual({1, 2}, {1, 2})
+
+	assert(isEqual, "Arrays aren't equal")
+end
+
+function TableUtilTest.comparesEqualNestedTables()
+	local sut = require(game.ReplicatedStorage.Utils.Table)
+
+	local isEqual = sut.tablesEqual({a={Hat=1}}, {a={Hat=1}})
+
+	assert(isEqual, "Tables aren't equal")
 end
 
 return TableUtilTest
